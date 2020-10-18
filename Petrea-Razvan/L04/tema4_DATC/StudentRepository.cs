501dc7798e4002bc732b8f4f13a80fae9326920c
RazvanPetrea98 <razvanpetrea98@gmail.com>
2020-10-18 16:43:45 +0300

    tema4

---
+++ Petrea-Razvan/L04/tema4_DATC/StudentRepository.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Models;
+
+namespace tema4_DATC
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private string _cconnectionString;
+        private CloudTableClient _tableClient;
+        private CloudTable _studentsTable;
+
+        public StudentRepository(IConfiguration configuration)
+        {
+            _cconnectionString = configuration.GetValue<string>("AzureStorageAccountConnectionString");
+            Task.Run(async () => { await InitizaleTable(); })
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public async Task CreateNewStudent(StudentEntity student)
+        {
+            var insertOperation = TableOperation.Insert(student);
+
+            await _studentsTable.ExecuteAsync(insertOperation);
+        }
+
+        public async Task<List<StudentEntity>> GetAllStudents()
+        {
+            var students = new List<StudentEntity>();
+            TableQuery<StudentEntity> query = new TableQuery<StudentEntity>();
+
+            TableContinuationToken token = null;
+            do
+            {
+                TableQuerySegment<StudentEntity> resultSegment = await _studentsTable.ExecuteQuerySegmentedAsync(query, token);
+                token = resultSegment.ContinuationToken;
+
+                students.AddRange(resultSegment.Results);
+            }  while (token != null);
+
+            return students;
+        }
+         
+          private async Task InitizaleTable()
+          {
+              
+            var account = CloudStorageAccount.Parse(_cconnectionString);
+            _tableClient = account.CreateCloudTableClient();
+
+            _studentsTable = _tableClient.GetTableReference("studenti");
+
+            await _studentsTable.CreateIfNotExistsAsync();
+          }
+    }
+}
